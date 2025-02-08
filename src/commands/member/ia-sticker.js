4148cef7c49c68b52958e99ebbe194a795f6c092
MRX <anonymous02669@gmail.com>
2025-02-08 00:19:06 -0300

    fix: substituí node-fetch por axios no comando ia-sticker, adicionei o script start.sh

--- src/commands/member/ia-sticker.js
+++ src/commands/member/ia-sticker.js
@@ -1,9 +1,9 @@
-const { PREFIX, SPIDER_API_TOKEN } = require(`${BASE_DIR}/config`);
-const fetch = require('node-fetch');
+const axios = require('axios');
+const { PREFIX, SPIDER_API_BASE_URL, SPIDER_API_TOKEN } = require(`${BASE_DIR}/config`);
 
 module.exports = {
   name: "ia-sticker",
-  description: "cria uma figurinha com base em uma descrição",
+  description: "Cria uma figurinha com base em uma descrição",
   commands: ["ia-sticker", "ia-fig"],
   usage: `${PREFIX}ia-sticker <descrição>`,
   handle: async ({
@@ -15,28 +15,27 @@ module.exports = {
     sendErrorReply
   }) => {
     if (!args[0]) {
-      return sendWarningReply("Você precisa fornecer uma descrição para a imagem.");
+      return sendWarningReply("Você precisa fornecer uma descrição para a figurinha.");
     }
 
-    const descricao = args[0];
-
+    const descricao = args.join(" ");
     const apiUrl = `https://api.spiderx.com.br/api/ai/stable-diffusion-turbo?search=${encodeURIComponent(descricao)}&api_key=${SPIDER_API_TOKEN}`;
 
-        sendWaitReply("Gerando figurinha...");
+    await sendWaitReply("Gerando figurinha...");
 
     try {
-      const response = await fetch(apiUrl);
-      const data = await response.json();
+      const response = await axios.get(apiUrl);
+      const data = response.data;
 
       if (data.image) {
-        sendStickerFromURL(data.image);
-        sendSuccessReact();
+        await sendStickerFromURL(data.image);
+        await sendSuccessReact();
       } else {
-        sendWarningReply("Não foi possível gerar a figurinha. Tente novamente mais tarde.");
+        await sendWarningReply("Não foi possível gerar a figurinha. Tente novamente mais tarde.");
       }
     } catch (error) {
       console.log(error);
-      sendErrorReply("Houve um erro ao tentar gerar a imagem. Verifique sua conexão ou tente mais tarde.");
+      await sendErrorReply("Houve um erro ao tentar gerar a imagem. Verifique sua conexão ou tente mais tarde.");
     }
   },
 };
