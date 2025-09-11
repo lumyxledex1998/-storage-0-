3ddb0716807d375660614ba38c4f18445e2a50d6
mineaplela-wq <mineaplela@gmail.com>
2025-09-11 13:30:48 -0300

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,10 +1,10 @@
 const path = require("path");
 
 // Prefixo padrão dos comandos.
-exports.PREFIX = "/";
+exports.PREFIX = "7";
 
 // Emoji do bot (mude se preferir).
-exports.BOT_EMOJI = "🤖";
+exports.BOT_EMOJI = "🍁";
 
 // Nome do bot (mude se preferir).
 exports.BOT_NAME = "Takeshi Bot";
@@ -12,12 +12,12 @@ exports.BOT_NAME = "Takeshi Bot";
 // Número do bot.
 // Apenas números, exatamente como está no WhatsApp.
 // Se o seu número não exibir o nono dígito (9) no WhatsApp, não coloque-o.
-exports.BOT_NUMBER = "558112345678";
+exports.BOT_NUMBER = "5584987777463";
 
 // Número do dono bot.
 // Apenas números, exatamente como está no WhatsApp.
 // Se o seu número não exibir o nono dígito (9) no WhatsApp, não coloque-o.
-exports.OWNER_NUMBER = "5521950502020";
+exports.OWNER_NUMBER = "5584987777463";
 
 // LID do dono do bot.
 // Para obter o LID do dono do bot, use o comando <prefixo>get-lid @marca ou +telefone do dono.
